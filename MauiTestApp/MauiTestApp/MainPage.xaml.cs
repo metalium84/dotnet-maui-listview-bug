bb472fa300feaa829f0f3fdb337b8195bbaa1a2b
Gabriel Zweifel <gabriel.zweifel@xappido.com>
2023-12-15 12:07:27 +0100

    Initial Commit

---
+++ MauiTestApp/MauiTestApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiTestApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ListView_OnRefreshing(object? sender, EventArgs e)
+    {
+        Thread.Sleep(1000);
+        this.MyListView.IsRefreshing = false;
+    }
+}
+
+public class MyTest : INotifyPropertyChanged
+{
+    static MyTest _Instance;
+    public static MyTest Instance => _Instance ?? new();
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public ObservableCollection<Option> Items { get; set; } =  new ()
+    {
+        new Option { Enabled = false, Name = "Option1"},
+        new Option { Enabled = false, Name = "Option2"},
+        new Option { Enabled = false, Name = "Option3"}
+    };
+}
+
+public class Option
+{
+    public string Name { get; set; }
+    
+    public bool Enabled {
+        get;
+        set;
+    }
+}
